e3faf15e2f08c54f45c346605d90f04b24bcf69f
Kacper Śmielak <96179878+Smieluuu@users.noreply.github.com>
2023-03-28 22:32:39 +0200

    Add Digest-Auth-Cracker

---
+++ DigestAuth/DigestAuthCracker.py
@@ -0,0 +1,35 @@
+from hashlib import md5, sha256, sha3_512, sha3_384, sha512, sha224, sha384, blake2b, blake2s 
+
+realm = "" # realm
+username = "" # username
+uri = "" # /path/to/file
+nonce = "" # nonce
+
+with open('rockyou.txt', 'r', encoding=("latin-1")) as f:
+    passwords = f.read().split("\n")
+
+funcions = [md5, sha256, sha3_512, sha3_384, sha512, sha224, sha384, blake2b, blake2s]
+methods = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'CONNECT', 'TRACE']
+
+for password in passwords:
+    for function in funcions:
+        for method in methods:
+            # username:realm:password
+            A1 = username + ':' + realm + ':' + password
+            A1 = function(A1.encode('utf-8')).hexdigest()
+
+            # method:uri
+            A2 = method + ':' + uri
+            A2 = function(A2.encode('utf-8')).hexdigest()
+
+            # response = function(A1:nonce:A2)
+            response = A1 + ':' + nonce + ':' + A2
+            response = function(response.encode('utf-8')).hexdigest()
+
+            if response == '': # response
+                print("Password found: " + password)
+                print("Hash: " + response)
+                print("Function: " + function.__name__)
+                print("Method: " + method)
+                break
+
